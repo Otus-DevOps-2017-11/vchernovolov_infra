84480ee8bbbc06d1624c0daada5555d81b86c182
Vladimir <vova._@mail.ru>
2018-01-11 21:30:58 +0300

    ДЗ-09 Принципы организации инфраструктурного кода (terraform)

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -6,26 +6,6 @@ data "template_file" "pumaservice" {
   }
 }
 
-/*
-data "template_file" "envvars" {
-  template = "${file("${path.module}/files/env_vars.sh.tpl")}"
-
-  vars {
-    db_addr = "${var.db_addr}"
-  }
-}
-*/
-
-/*
-data "template_file" "deploy" {
-  template = "${file("${path.module}/files/deploy.sh.tpl")}"
-
-  vars {
-    db_addr = "${var.db_addr}"
-  }
-}
-*/
-
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "${var.machine_type}"
@@ -56,42 +36,6 @@ resource "google_compute_instance" "app" {
     agent       = false
     private_key = "${file(var.private_key_path)}"
   }
-
-/*
-  provisioner "file" {
-    source = "${path.module}/files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-*/
-
-/*
-  provisioner "file" {
-    content      = "${data.template_file.envvars.rendered}"
-    destination = "/tmp/env_vars.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/env_vars.sh",
-      "/tmp/env_vars.sh",
-      "echo done > /tmp/env_done.txt",
-    ]
-  }
-*/
-
-/*
-  provisioner "file" {
-    content      = "${data.template_file.deploy.rendered}"
-    destination = "/tmp/deploy.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/deploy.sh",
-      "/tmp/deploy.sh",
-    ]
-  }
-*/
 
   provisioner "file" {
     content     = "${data.template_file.pumaservice.rendered}"
